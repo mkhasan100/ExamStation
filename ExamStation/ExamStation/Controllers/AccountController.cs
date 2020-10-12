8233045b0b52ba56f38ef94717d6c6a8948480f4
hasan3848 <hasan3848@gmail.com>
2020-10-12 09:35:08 +0600

    initial

---
+++ ExamStation/ExamStation/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamStation.Areas.Identity.Data;
+using ExamStation.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamStation.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ExamStationUser> _signInManager;
+        private readonly UserManager<ExamStationUser> _userManager;
+        public AccountController(
+            UserManager<ExamStationUser> userManager,
+            SignInManager<ExamStationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            //_logger.LogInformation("User logged out.");
+            //return RedirectToPage("/Identity/Account/Login");
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                // ChangePasswordAsync changes the user password
+                var result = await _userManager.ChangePasswordAsync(user,
+                    model.OldPassword, model.NewPassword);
+
+                // The new password did not meet the complexity rules or
+                // the current password is incorrect. Add these errors to
+                // the ModelState and rerender ChangePassword view
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                // Upon successfully changing the password refresh sign-in cookie
+                await _signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ ExamStation/ExamStation/Data/ExamStationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExamStation.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ExamStationUser class
+    public class ExamStationUser : IdentityUser
+    {
+    }
+}
